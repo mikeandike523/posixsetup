d10bfedc8f8c44db742b9e6cbf6bf018e94fb1ad
Michael Sohnen <michaelsohnenacademic@gmail.com>
2024-05-01 12:55:31 -0400

    Added git stash directives

--- updater.sh
+++ updater.sh
@@ -3,18 +3,22 @@ sudo apt upgrade -y
 
 cd ~/.local/share
 
-cd gitsleuth
+rm -rf gitsleuth
+git stash
 git pull --force
 cd ..
 
 cd tsleuth 
+git stash
 git pull --force
 cd ..
 
 cd opkvs
+git stash
 git pull --force
 cd ..
 
 cd eolinuxify
+git stash
 git pull --force
 cd ..
