7a09110dbee2a6e26ceea54c05df9d81f5d84cd6
Michael Sohnen <michaelsohnenacademic@gmail.com>
2024-05-21 16:53:53 -0400

    progress

--- setup.sh
+++ setup.sh
@@ -18,6 +18,12 @@ echo "Found platform: "$platform""
 
 if [ "$platform" = "linux" ] || [ "$platform" = "wsl" ]; then
     set +e
+    if [ -n "$WSL_DISTRO_NAME" ]; then
+        echo "WSL detected, proceeding with installation of wslu."
+        sudo apt install -y wslu
+    else
+        echo "Not running under WSL, skipping installation of wslu."
+    fi
     sudo apt update -y
     sudo apt upgrade -y
     sudo apt-get install build-essential
@@ -38,8 +44,33 @@ brew install git
 brew install gcc
 brew install docker docker-compose
 brew install gh
-brew install --cask 1password-cli
 brew install node
+
+function install_1password_cli_linux {
+        
+    sudo rm -f /usr/share/keyrings/1password-archive-keyring.gpg
+    sudo rm -f /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+    sudo curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+    sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+    sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
+    sudo tee /etc/apt/sources.list.d/1password.list
+    sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+    sudo curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
+    sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+    sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+    sudo curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+    sudo sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+    sudo apt update && sudo apt install 1password-cli
+}
+
+
+if [ "$platform" = "linux" ] || [ "$platform" = "wsl" ]; then
+    install_1password_cli_linux
+else
+    brew install --cask 1password-cli
+fi
+
+
 
 # Is idempotent, or will update to ltest version,
 # so its okay to not check
